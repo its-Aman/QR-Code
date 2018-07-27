594b881d7b6a00565d86d28e6b7de068557e9592
Aman Kumar <kumaraman7056@gmail.com>
2018-07-27 23:52:05 +0530

    updatd

--- src/pages/select-active-event/select-active-event.ts
+++ src/pages/select-active-event/select-active-event.ts
@@ -144,8 +144,11 @@ export class SelectActiveEventPage {
     if (this.selectEventForm.valid) {
       this.global.cLog('form is valid');
       let data = this.eventList.find(val => val.id == this.selectEventForm.controls['event'].value);
-      this.db.create('event-selected', data).then((ev) => { this.global.cLog(`event-selected done`, ev) });
-      this.navCtrl.setRoot('MenuPage', { data: data });
+      this.db.create('event-selected', data)
+        .then((ev) => {
+          this.global.cLog(`event-selected done`, ev);
+          this.navCtrl.setRoot('MenuPage', { data: data });
+        });
     } else {
       this.isFormInvalid = true;
     }
